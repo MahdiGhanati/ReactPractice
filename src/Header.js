d4419f5a90284d77520bf03bdb283376a512b1d5
unknown <mghanaty@gmail.com>
2024-03-18 23:22:31 +0330

    update Deploy 0

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-                Mahdi Ghanati Deploy Test
+                Mahdi Ghanati Deploy Test 0
             </div>
         );
     }
